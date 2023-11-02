df90c1c85416032a1fb95796afed8e1dac8eb753
victoriakapelush <victoriakapelush@gmail.com>
2023-11-01 20:17:43 -0400

    finish form

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('button').onclick = function() {
-    if (document.getElementById('firstName').value.length < 1) {
-        document.getElementById('firstName-container').style.borderColor = "red";
-        return false;
-    } else if (document.getElementById('lastName').value.length < 1) {
-        document.getElementById('lastName-container').style.borderColor = "red";
-        return false;
-    }
-}
